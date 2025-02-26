6d71a6e64bcce8a431baade8a99d0fa6f1cc439f
aryans41 <aryans41@nycstudents.net>
2025-02-26 17:31:03 +0000

    second commit

--- src/main/java/com/example/project/Insertion_Sort/InsertionSort.java
+++ src/main/java/com/example/project/Insertion_Sort/InsertionSort.java
@@ -8,24 +8,68 @@ public class InsertionSort {
 
     // PART A. implementing insertion sort
     public static int[] insertionSort(int[] elements) {
-       
+        for (int i = 1; i < elements.length; i++) {
+            int currentValue = elements[i];
+            int j = i;
+            while (j > 0 && elements[j - 1] > currentValue) {
+                elements[j] = elements[j - 1];
+                j--;
+            }
+            elements[j] = currentValue;
+        }
         return elements;
     }
 
-   
     public static void selectionSort(int[] elements) {
-    
+        int minimumIndex = 0;  
+        int temp;  
+        for (int i = 0; i < elements.length; i++) {
+            minimumIndex = i;
+            for (int x = i + 1; x < elements.length; x++) {
+                if (elements[x] < elements[minimumIndex]) {
+                    minimumIndex = x;
+                }
+            }
+            temp = elements[i];
+            elements[i] = elements[minimumIndex];
+            elements[minimumIndex] = temp;
+       }
     }
 
     // PART B. sorting a 1000-word list
     public static ArrayList<String> insertionSortWordList(ArrayList<String> words) {
-    
+        int count = 0;
+        for (int i = 1; i < words.size(); i++) {
+            String currentWord = words.get(i);
+            int j = i;
+            while (j > 0 && words.get(j - 1).compareTo(currentWord) > 0) {
+                count++;
+                words.set(j, words.get(j - 1));
+                j--;
+            }
+            words.set(j, currentWord);
+        }
+        System.out.println("INSERTION SORT: Number of loop iterations: " + count);
         return words;
     }
 
+
     public static void selectionSortWordList(ArrayList<String> words) {
-    
+        int count = 0;
+        for (int i = 0; i < words.size(); i++) {
+            int minimumIndex = i;
+            for (int x = i + 1; x < words.size(); x++) {
+                count++;
+                if (words.get(x).compareTo(words.get(minimumIndex)) < 0) {
+                    minimumIndex = x;
+                }
+            }
+            String temp = words.set(i, words.get(minimumIndex));
+            words.set(minimumIndex, temp);
+            System.out.println("SELECTION SORT: Number of loop iterations: " + count);
+       }
     }
+
 
     public static ArrayList<String> loadWordsInto(ArrayList<String> wordList) {
         try {
